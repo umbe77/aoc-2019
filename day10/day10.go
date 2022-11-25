cd4ce4d0b2d421736077c24e842e2939628c9c3d
Roberto Ughi <umbe77@gmail.com>
2022-11-25 01:49:48 +0100

    Day 10 Part 1 part 2 require trigonometry

--- day10/day10.go
+++ day10/day10.go
@@ -2,10 +2,11 @@ package day10
 
 import (
 	"fmt"
-	"sort"
 
 	"github.com/umbe77/aoc-2019/utils"
 )
+
+var EPSILON float64 = 0.000001
 
 func Execute() {
 	matrix := make([]string, 0)
@@ -13,19 +14,29 @@ func Execute() {
 		matrix = append(matrix, line)
 	})
 
-	matrix1 := []string{
-		"......#.#.",
-		"#..#.#....",
-		"..#######.",
-		".#.#.###..",
-		".#..#.....",
-		"..#....#.#",
-		"#..#....#.",
-		".##.#..###",
-		"##...#..#.",
-		".#....####",
-	}
-	asteroids := ParseMapLine(matrix1)
+	// matrix1 := []string{
+	// 	".#..##.###...#######",
+	// 	"##.############..##.",
+	// 	".#.######.########.#",
+	// 	".###.#######.####.#.",
+	// 	"#####.##.#.##.###.##",
+	// 	"..#####..#.#########",
+	// 	"####################",
+	// 	"#.####....###.#.#.##",
+	// 	"##.#################",
+	// 	"#####.##.###..####..",
+	// 	"..######..##.#######",
+	// 	"####.##.####...##..#",
+	// 	".#####..#.######.###",
+	// 	"##...#.##########...",
+	// 	"#.##########.#######",
+	// 	".####.#.###.###.#.##",
+	// 	"....##.##.###..#####",
+	// 	".#.#.###########.###",
+	// 	"#.#.#.#####.####.###",
+	// 	"###.##.####.##.#..##",
+	// }
+	asteroids := ParseMapLine(matrix)
 	fmt.Printf("Part 1: %d\n", BestAsteroid(asteroids))
 }
 
@@ -45,82 +56,53 @@ func ParseMapLine(matrix []string) []Asteroid {
 	return asteroids
 }
 
-func checkOnSameLine(a, b, c Asteroid) bool {
-	dx := a.X - b.X
-	dy := a.Y - b.Y
-	dx1 := a.X - c.X
-	dy1 := a.Y - c.Y
-	return dx1*dy == dy1*dx
-}
-
 type Asteroid struct {
 	X int
 	Y int
 }
 
-func isBetween(a, p1, p2 Asteroid) bool {
-	dxl := p1.X - p2.X
-	dyl := p1.Y - p2.Y
-	if utils.Abs(dxl) >= utils.Abs(dyl) {
-		if dxl > 0 {
-			return p1.X <= a.X && a.X <= p2.X
-		} else {
-			return p2.X <= a.X && a.X <= p1.X
-		}
-	} else {
-		if dyl > 0 {
-			return p1.Y <= a.Y && a.Y <= p2.Y
-		} else {
-			return p2.Y <= a.Y && a.Y <= p1.Y
-		}
-	}
-}
-
 func BestAsteroid(asteroids []Asteroid) int {
-	locations := make(map[string][]Asteroid)
-	for _, a := range asteroids {
-		name := fmt.Sprintf("%d%d", a.X, a.Y)
-		locations[name] = make([]Asteroid, 0)
-		inLine := make([]Asteroid, 0)
-		for _, b := range asteroids {
-			if a.X == b.X && a.Y == a.Y {
-				continue
-			}
-			inLine = append(inLine, b)
-			for _, c := range asteroids {
-				if (a.X == c.X && a.Y == c.Y) ||
-					b.X == c.X && b.Y == c.Y {
-					continue
-				}
-				if checkOnSameLine(a, b, c) {
-					inLine = append(inLine, c)
+	maxCountVisible := 0
+	for _, asteroid := range asteroids {
+		leftQuadrant, rightQuadrant := make(map[float64]bool), make(map[float64]bool)
+		var checkVerticalUp, checkVerticalDown bool
+		count := 0
+		for _, other := range asteroids {
+			if asteroid != other {
+				dy := other.Y - asteroid.Y
+				dx := other.X - asteroid.X
+				if dx == 0 {
+					if dy > 0 {
+						if !checkVerticalUp {
+							checkVerticalUp = true
+							count++
+						}
+					} else {
+						if !checkVerticalDown {
+							checkVerticalDown = true
+							count++
+						}
+					}
+				} else {
+					ratio := float64(dy) / float64(dx)
+					if dx < 0 {
+						if _, inLeft := leftQuadrant[ratio]; !inLeft {
+							leftQuadrant[ratio] = true
+							count++
+						}
+					} else {
+						if _, inRight := rightQuadrant[ratio]; !inRight {
+							rightQuadrant[ratio] = true
+							count++
+						}
+					}
 				}
 			}
 		}
-		sort.Slice(inLine, func(i, j int) bool {
-			return inLine[i].X <= inLine[j].X && inLine[i].Y <= inLine[j].Y
-		})
-		if a.X <= inLine[0].X && a.Y <= inLine[0].Y {
-			locations[name] = append(locations[name], inLine[0])
-		} else if a.X >= inLine[len(inLine)-1].X && a.Y >= inLine[len(inLine)-1].Y {
-			locations[name] = append(locations[name], inLine[len(inLine)-1])
-		} else {
-			for i := 1; i < len(inLine); i++ {
-				p1 := inLine[i-1]
-				p2 := inLine[i]
-				if isBetween(a, p1, p2) {
-					locations[name] = append(locations[name], p1)
-					locations[name] = append(locations[name], p2)
-					break
-				}
-			}
+		if maxCountVisible < count {
+			maxCountVisible = count
 		}
 	}
-	fmt.Printf("%+v\n", locations)
-	astList := make([]int, 0)
-	for _, l := range locations {
-		astList = append(astList, len(l))
-	}
-	sort.Ints(astList)
-	return astList[len(astList)-1]
+
+	return maxCountVisible
 }
