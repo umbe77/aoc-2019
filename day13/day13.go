e2d60a119519cc82475ae483dfb363b0ec302b9b
Roberto Ughi <umbe77@gmail.com>
2022-11-27 20:38:19 +0100

    Part 13

--- day13/day13.go
+++ day13/day13.go
@@ -2,6 +2,7 @@ package day13
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/umbe77/aoc-2019/intcode"
 	"github.com/umbe77/aoc-2019/utils"
@@ -14,6 +15,52 @@ func Execute() {
 	})
 
 	fmt.Printf("Part 1: %d\n", Part1(data))
+	fmt.Println("Part 2:")
+	game := RunGame(data, true)
+	DrawGame(game)
+}
+
+func Part1(code string) int {
+	count := 0
+	for _, v := range RunGame(code, false) {
+		if v == Block {
+			count++
+		}
+	}
+	return count
+}
+
+func DrawGame(game map[Cell]TileKind) {
+	xs := make([]int, 0)
+	ys := make([]int, 0)
+	for k := range game {
+		xs = append(xs, k.X)
+		ys = append(xs, k.Y)
+	}
+	sort.Ints(xs)
+	sort.Ints(ys)
+	maxY := ys[len(ys)-1]
+	maxX := xs[len(xs)-1]
+
+	for y := 0; y <= maxY; y++ {
+		for x := 0; x <= maxX; x++ {
+			v := game[Cell{X: x, Y: y}]
+			switch v {
+			case Empty:
+				fmt.Print(" ")
+			case Wall:
+				fmt.Print("█")
+			case Block:
+				fmt.Print(".")
+			case Paddle:
+				fmt.Print("-")
+			case Ball:
+				fmt.Print("o")
+			}
+		}
+		fmt.Println()
+	}
+
 }
 
 type TileKind int
@@ -26,18 +73,28 @@ const (
 	Ball
 )
 
-func Part1(code string) int {
+type Cell struct {
+	X int
+	Y int
+}
+
+func RunGame(code string, quarter bool) map[Cell]TileKind {
+	game := make(map[Cell]TileKind)
 	ic := intcode.New(code)
-	count := 0
+	if quarter {
+		ic.Set(0, 2)
+	}
 	for *ic.Status == intcode.Running {
-		ic.Run([]int{})
-		ic.Run([]int{})
+		x := ic.Run([]int{0})
+		y := ic.Run([]int{1})
 		id := ic.Run([]int{})
 
-		if id == int(Block) {
-			count++
+		if x == -1 && y == 0 {
+			fmt.Println(id)
 		}
 
+		game[Cell{X: x, Y: y}] = TileKind(id)
+
 	}
-	return count
+	return game
 }
